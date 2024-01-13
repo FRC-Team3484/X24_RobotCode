1fca150396aaad37d5f69ff3fa171fde1a09f454
FRC3484_PRGDS <frc3484@gmail.com>
2024-01-13 16:00:52 -0500

    ShooterCode work on CPP file and fix the includes

--- 2024Robot_Team3484/src/main/include/Constants.h
+++ 2024Robot_Team3484/src/main/include/Constants.h
@@ -1,7 +1,14 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-namespace ShooterConstants {}
+namespace ShooterConstants {
+    #define MOTOR_LEFT_PORT 0
+    #define MOTOR_RIGHT_PORT 1
+    #define P 0
+    #define I 0
+    #define D 0
+
+}
 namespace IntakeConstants {}
 namespace HookConstants {}
 
