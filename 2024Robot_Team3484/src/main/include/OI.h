158034f466cfb958c81eab596083b72e42542428
FRC3484_PRGDS <frc3484@gmail.com>
2024-01-16 20:32:47 -0500

    Shooter with start of Operator OI

--- 2024Robot_Team3484/src/main/include/OI.h
+++ 2024Robot_Team3484/src/main/include/OI.h
@@ -4,5 +4,18 @@
 
 #include "Constants.h"
 
+#include <frc/XboxController.h>
 
+class Operator_OI{
+    public:
+        bool DeployIntake();
+        bool Launch();
+        bool Climb();
+        bool IgnoreSensor();
+        bool IgnoreVison();
+       
+    private:
+        //frc::XboxController _Operator_controller{SwerveConstants::ControllerConstants::OPERATOR_CONTROLLER_PORT};
+
+};
 #endif
