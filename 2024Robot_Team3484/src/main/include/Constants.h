829d7ff9ccd5d949b0cc39c9ead89e4a483c2d83
nfoert <nofoert@gmail.com>
2024-01-22 19:18:27 -0500

    Finish claw subsystem

--- 2024Robot_Team3484/src/main/include/Constants.h
+++ 2024Robot_Team3484/src/main/include/Constants.h
@@ -3,21 +3,15 @@
 
 namespace ShooterConstants {}
 namespace IntakeConstants {}
-namespace HookConstants {
+namespace ClawConstants {
     constexpr int LEFT_MOTOR_CAN_ID = 50;
     constexpr int RIGHT_MOTOR_CAN_ID = 51;
     constexpr int LEFT_SENSOR_DI_CH= 1;
     constexpr int RIGHT_SENSOR_DI_CH= 1;
 
     constexpr bool MOTOR_INVERTED = false;
+    constexpr int MOTOR_STOP = 0;
 }
-
-
-
-
-
-
-
 
 namespace SwerveConstants {}
 
