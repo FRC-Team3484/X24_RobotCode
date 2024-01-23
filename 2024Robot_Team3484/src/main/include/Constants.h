81cd071cf20bd9eb165d28f8190491df3eb59376
nfoert <nofoert@gmail.com>
2024-01-22 20:08:20 -0500

    Add DigitalInput channels

--- 2024Robot_Team3484/src/main/include/Constants.h
+++ 2024Robot_Team3484/src/main/include/Constants.h
@@ -6,8 +6,8 @@ namespace IntakeConstants {}
 namespace ClawConstants {
     constexpr int LEFT_MOTOR_CAN_ID = 50;
     constexpr int RIGHT_MOTOR_CAN_ID = 51;
-    constexpr int LEFT_SENSOR_DI_CH= 1;
-    constexpr int RIGHT_SENSOR_DI_CH= 1;
+    constexpr int LEFT_SENSOR_DI_CH= 3;
+    constexpr int RIGHT_SENSOR_DI_CH= 4;
 
     constexpr bool MOTOR_INVERTED = false;
     constexpr int MOTOR_STOP = 0;
