6aaf87adb6868aa035c90b6dfd7c4e5db266fde2
FRC3484_PRGDS <frc3484@gmail.com>
2024-02-07 19:40:21 -0500

    Fixed naming of climber from claw
    '

--- 2024Robot_Team3484/src/main/include/Constants.h
+++ 2024Robot_Team3484/src/main/include/Constants.h
@@ -55,7 +55,7 @@ namespace IntakeConstants {
     constexpr units::degree_t POSITION_TOLERANCE = 2_deg;
 }
 
-namespace ClawConstants {
+namespace ClimberConstants {
     constexpr int LEFT_MOTOR_CAN_ID = 50;
     constexpr int RIGHT_MOTOR_CAN_ID = 51;
     constexpr int LEFT_SENSOR_DI_CH = 3;
--- 2024Robot_Team3484/src/main/include/subsystems/ClimberSubsystem.h
+++ 2024Robot_Team3484/src/main/include/subsystems/ClimberSubsystem.h
@@ -1,5 +1,5 @@
-#ifndef CLAWSUBSYSTEM_H
-#define CLAWSUBSYSTEM_H
+#ifndef CLIMBERSUBSYSTEM_H
+#define CLIMBERSUBSYSTEM_H
 
 #include <Constants.h>
 
@@ -7,22 +7,22 @@
 #include <rev/CANSparkMax.h>
 #include <frc/DigitalInput.h>
 
-class ClawSubsystem : public frc2::SubsystemBase {
+class ClimberSubsystem : public frc2::SubsystemBase {
     public: 
-        ClawSubsystem(
+        ClimberSubsystem(
             int left_motor_can_id,
             int right_motor_can_id,
             int left_sensor_di_ch,
             int right_sensor_di_ch
         );
         void Periodic() override;
-        void SetClawPower(double power);
+        void SetClimberPower(double power);
         bool GetLeftSensor();
         bool GetRightSensor();
 
     private: 
-        rev::CANSparkMax _left_climb_motor;
-        rev::CANSparkMax _right_climb_motor;
+        rev::CANSparkMax _left_climber_motor;
+        rev::CANSparkMax _right_climber_motor;
 
         frc::DigitalInput _left_motor_sensor;
         frc::DigitalInput _right_motor_sensor;
