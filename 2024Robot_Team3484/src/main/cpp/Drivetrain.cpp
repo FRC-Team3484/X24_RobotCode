818f44c07add60ea3bdfcdf886c7ae7dfa843a25
michamichaa <116735133+michamichaa@users.noreply.github.com>
2024-03-06 18:41:24 -0500

    Will recheck the tuning

--- 2024Robot_Team3484/src/main/cpp/Drivetrain.cpp
+++ 2024Robot_Team3484/src/main/cpp/Drivetrain.cpp
@@ -3,20 +3,147 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/Drivetrain.h"
+#include "ctre/phoenix/motorcontrol/NeutralMode.h"
+#include "units/angle.h"
 
 #include <frc2/command/Commands.h>
 
-Drive::Drive() {
- _drive_motor_BR.Follow(_drive_motor_FR);
- _drive_motor_BL.Follow(_drive_motor_FL);
-//  _drive_motor_BL.SetInverted(false);
- _drive_motor_FL.SetInverted(false);
-//  _drive_motor_BR.SetInverted(false);
- _drive_motor_FR.SetInverted(false);
+using namespace units;
+using namespace ctre::phoenix::sensors;
+using namespace ctre::phoenix;
 
- m_drive.SetSafetyEnabled(false);
+Drive::Drive() {
+
+    _drive_motor_BR.ConfigFactoryDefault();
+    _drive_motor_BL.ConfigFactoryDefault();
+    _drive_motor_FR.ConfigFactoryDefault();
+    _drive_motor_FL.ConfigFactoryDefault();
+    
+    _drive_motor_BR.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _drive_motor_BL.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _drive_motor_FR.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _drive_motor_FL.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+
+    _steer_motor_BR.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _steer_motor_BL.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _steer_motor_FR.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+    _steer_motor_FL.SetNeutralMode(ctre::phoenix::motorcontrol::Brake);
+
+     _drive_motor_BR.Follow(_drive_motor_FR);
+     _drive_motor_BL.Follow(_drive_motor_FL);
+    //  _drive_motor_BL.SetInverted(false);
+    //  _drive_motor_FL.SetInverted(false);
+    // //  _drive_motor_BR.SetInverted(false);
+    //  _drive_motor_FR.SetInverted(false);
+
+     m_drive.SetSafetyEnabled(false);
+
+    _steer_motor_FL.ConfigFactoryDefault();
+    _steer_motor_FL.SetNeutralMode(motorcontrol::Brake);
+    _steer_motor_FL.ConfigSupplyCurrentLimit(_steer_current_limit);
+    _steer_motor_FL.SetInverted(true);
+
+    _steer_motor_FL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_1_General_, 255);
+    _steer_motor_FL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_4_AinTempVbat_, 255);
+    _steer_motor_FL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_12_Feedback1_, 255);
+    _steer_motor_FL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_14_Turn_PIDF1_, 200);
+    
+
+    _encoder_FL.ConfigFactoryDefault();
+    _encoder_FL.SetPositionToAbsolute();
+    _encoder_FL.ConfigAbsoluteSensorRange(sensors::AbsoluteSensorRange::Signed_PlusMinus180);
+    _encoder_FL.ConfigSensorInitializationStrategy(SensorInitializationStrategy::BootToAbsolutePosition);
+    _encoder_FL.ConfigMagnetOffset(-92.505);
+    _encoder_FL.ConfigSensorDirection(false);
+    _encoder_FL.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_SensorData, 20);
+    _encoder_FL.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_VbatAndFaults, 200);
+    _steer_pid_controller_FL.EnableContinuousInput(-180_deg, 180_deg);
+    
+    _steer_motor_FR.ConfigFactoryDefault();
+    _steer_motor_FR.SetNeutralMode(motorcontrol::Brake);
+    _steer_motor_FR.ConfigSupplyCurrentLimit(_steer_current_limit);
+    _steer_motor_FR.SetInverted(true);
+
+    _steer_motor_FR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_1_General_, 255);
+    _steer_motor_FR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_4_AinTempVbat_, 255);
+    _steer_motor_FR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_12_Feedback1_, 255);
+    _steer_motor_FR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_14_Turn_PIDF1_, 200);
+    
+
+    _encoder_FR.ConfigFactoryDefault();
+    _encoder_FR.SetPositionToAbsolute();
+    _encoder_FR.ConfigAbsoluteSensorRange(sensors::AbsoluteSensorRange::Signed_PlusMinus180);
+    _encoder_FR.ConfigSensorInitializationStrategy(SensorInitializationStrategy::BootToAbsolutePosition);
+    _encoder_FR.ConfigMagnetOffset(-60.205);
+    _encoder_FR.ConfigSensorDirection(false);
+    _encoder_FR.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_SensorData, 20);
+    _encoder_FR.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_VbatAndFaults, 200);
+    _steer_pid_controller_FR.EnableContinuousInput(-180_deg, 180_deg);
+
+    _steer_motor_BR.ConfigFactoryDefault();
+    _steer_motor_BR.SetNeutralMode(motorcontrol::Brake);
+    _steer_motor_BR.ConfigSupplyCurrentLimit(_steer_current_limit);
+    _steer_motor_BR.SetInverted(true);
+
+    _steer_motor_BR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_1_General_, 255);
+    _steer_motor_BR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_4_AinTempVbat_, 255);
+    _steer_motor_BR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_12_Feedback1_, 255);
+    _steer_motor_BR.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_14_Turn_PIDF1_, 200);
+    
+
+    _encoder_BR.ConfigFactoryDefault();
+    _encoder_BR.SetPositionToAbsolute();
+    _encoder_BR.ConfigAbsoluteSensorRange(sensors::AbsoluteSensorRange::Signed_PlusMinus180);
+    _encoder_BR.ConfigSensorInitializationStrategy(SensorInitializationStrategy::BootToAbsolutePosition);
+    _encoder_BR.ConfigMagnetOffset(-55.283);
+    _encoder_BR.ConfigSensorDirection(false);
+    _encoder_BR.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_SensorData, 20);
+    _encoder_BR.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_VbatAndFaults, 200);
+    _steer_pid_controller_BR.EnableContinuousInput(-180_deg, 180_deg);
+    
+    _steer_motor_BL.ConfigFactoryDefault();
+    _steer_motor_BL.SetNeutralMode(motorcontrol::Brake);
+    _steer_motor_BL.ConfigSupplyCurrentLimit(_steer_current_limit);
+    _steer_motor_BL.SetInverted(true);
+
+    _steer_motor_BL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_1_General_, 255);
+    _steer_motor_BL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_4_AinTempVbat_, 255);
+    _steer_motor_BL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_12_Feedback1_, 255);
+    _steer_motor_BL.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrame::Status_14_Turn_PIDF1_, 200);
+    
+
+    _encoder_BL.ConfigFactoryDefault();
+    _encoder_BL.SetPositionToAbsolute();
+    _encoder_BL.ConfigAbsoluteSensorRange(sensors::AbsoluteSensorRange::Signed_PlusMinus180);
+    _encoder_BL.ConfigSensorInitializationStrategy(SensorInitializationStrategy::BootToAbsolutePosition);
+    _encoder_BL.ConfigMagnetOffset(160.654);
+    _encoder_BL.ConfigSensorDirection(false);
+    _encoder_BL.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_SensorData, 20);
+    _encoder_BL.SetStatusFramePeriod(ctre::phoenix::sensors::CANCoderStatusFrame::CANCoderStatusFrame_VbatAndFaults, 200);
+    _steer_pid_controller_BL.EnableContinuousInput(-180_deg, 180_deg);
+}
+
+ 
+
+
+
+void Drive::Periodic() {
+
+
+    double steer_output_FR = _steer_pid_controller_FR.Calculate(GetSteerAngleFR(), 0_rad);
+    _steer_motor_FR.Set(steer_output_FR);
+
+    double steer_output_FL = _steer_pid_controller_FL.Calculate(GetSteerAngleFL(), 0_rad);
+    _steer_motor_FL.Set(steer_output_FL);
+
+    double steer_output_BL = _steer_pid_controller_BL.Calculate(GetSteerAngleBL(), 0_rad);
+    _steer_motor_BL.Set(steer_output_BL);
+
+    double steer_output_BR = _steer_pid_controller_BR.Calculate(GetSteerAngleBR(), 0_rad);
+    _steer_motor_BR.Set(steer_output_BR);
 
 }
+
 
 frc2::CommandPtr Drive::PseudoDriveCommand(std::function<double()> fwd,
                                            std::function<double()> rot) {
@@ -31,4 +158,18 @@ frc2::CommandPtr Drive::SysIdQuasistatic(frc2::sysid::Direction direction) {
 
 frc2::CommandPtr Drive::SysIdDynamic(frc2::sysid::Direction direction) {
   return m_sysIdRoutine.Dynamic(direction);
+}
+
+
+degree_t Drive::GetSteerAngleFL() {
+    return degree_t{_encoder_FL.GetAbsolutePosition()};
+}
+degree_t Drive::GetSteerAngleFR() {
+    return degree_t{_encoder_FR.GetAbsolutePosition()};
+}
+degree_t Drive::GetSteerAngleBL() {
+    return degree_t{_encoder_BL.GetAbsolutePosition()};
+}
+degree_t Drive::GetSteerAngleBR() {
+    return degree_t{_encoder_BR.GetAbsolutePosition()};
 }
--- 2024Robot_Team3484/src/main/include/subsystems/Drivetrain.h
+++ 2024Robot_Team3484/src/main/include/subsystems/Drivetrain.h
@@ -10,8 +10,15 @@
 #include <frc/RobotController.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc2/command/SubsystemBase.h>
-#include "ctre/phoenix/motorcontrol/can/WPI_TalonFX.h"
 #include <frc2/command/sysid/SysIdRoutine.h>
+#include <frc/controller/ProfiledPIDController.h>
+#include <units/length.h>
+
+#include <frc/controller/PIDController.h>
+
+// #include "ctre/phoenix/motorcontrol/SupplyCurrentLimitConfiguration.h"
+#include "ctre/phoenix/motorcontrol/can/WPI_TalonFX.h"
+#include "ctre/phoenix/sensors/WPI_CANCoder.h"
 
 
 
@@ -23,16 +30,52 @@ class Drive : public frc2::SubsystemBase {
                                       std::function<double()> rot);
   frc2::CommandPtr SysIdQuasistatic(frc2::sysid::Direction direction);
   frc2::CommandPtr SysIdDynamic(frc2::sysid::Direction direction);
+  
+  void Periodic() override;
+  // Steer Angles
+  units::degree_t GetSteerAngleFL();
+  units::degree_t GetSteerAngleFR();
+  units::degree_t GetSteerAngleBL();
+  units::degree_t GetSteerAngleBR();
+  
 
  private:
 //   frc::PWMSparkMax m_leftMotor{constants::drive::kLeftMotor1Port};
 //   frc::PWMSparkMax m_rightMotor{constants::drive::kRightMotor1Port};
-ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_FL{10};
-ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_FR{12};
-ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_BL{14};
-ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_BR{16};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_FL{10};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_FR{12};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_BL{14};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _drive_motor_BR{16};
+
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _steer_motor_FL{11};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _steer_motor_FR{13};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _steer_motor_BL{15};
+    ctre::phoenix::motorcontrol::can::WPI_TalonFX _steer_motor_BR{17};
+
+    ctre::phoenix::sensors::WPI_CANCoder _encoder_FL{20};
+    ctre::phoenix::sensors::WPI_CANCoder _encoder_FR{21};
+    ctre::phoenix::sensors::WPI_CANCoder _encoder_BL{22};
+    ctre::phoenix::sensors::WPI_CANCoder _encoder_BR{23};
+
+    // PID Loops
+
+    // frc::PIDController _drive_pid_controller{1.0,0.0,0.0};
+
+    frc::ProfiledPIDController<units::radians> _steer_pid_controller_FL{.5, 0.0, 0.0, 
+        {12_rad_per_s, 100_rad_per_s_sq}};
+    frc::ProfiledPIDController<units::radians> _steer_pid_controller_FR{.5, 0.0, 0.0, 
+        {12_rad_per_s, 100_rad_per_s_sq}};
+    frc::ProfiledPIDController<units::radians> _steer_pid_controller_BL{.5, 0.0, 0.0, 
+        {12_rad_per_s, 100_rad_per_s_sq}};
+    frc::ProfiledPIDController<units::radians> _steer_pid_controller_BR{.5, 0.0, 0.0, 
+        {12_rad_per_s, 100_rad_per_s_sq}};
+    ctre::phoenix::motorcontrol::SupplyCurrentLimitConfiguration _steer_current_limit{
+        true, 25, 40, 0.1
+    };
+
 frc::DifferentialDrive m_drive{[this](auto val) { _drive_motor_FL.Set(val); },
-                                [this](auto val) { _drive_motor_FR.Set(val); }};
+                                [this](auto val) { _drive_motor_FR.Set(val); }
+};
 
 
   frc2::sysid::SysIdRoutine m_sysIdRoutine{
@@ -56,19 +99,22 @@ frc::DifferentialDrive m_drive{[this](auto val) { _drive_motor_FL.Set(val); },
                          frc::RobotController::GetBatteryVoltage())
                 .position(units::meter_t{_drive_motor_FR.GetSelectedSensorPosition()*600/(36000.0/5880.0)})
                 .velocity(units::meters_per_second_t{_drive_motor_FR.GetSelectedSensorVelocity()*600/(36000.0/5880.0)});
-            // log->Motor("drive-bl")
-            //     .voltage(_drive_motor_BL.Get() *
-            //              frc::RobotController::GetBatteryVoltage())
-            //     .position(units::meter_t{_drive_motor_FL.GetSelectedSensorPosition()*600/(36000.0/5880.0)})
-            //     .velocity(units::meters_per_second_t{_drive_motor_FL.GetSelectedSensorVelocity()*600/(36000.0/5880.0)});
-            // log->Motor("drive-br")
-            //     .voltage(_drive_motor_BR.Get() *
-            //              frc::RobotController::GetBatteryVoltage())
-            //     .position(units::meter_t{_drive_motor_FL.GetSelectedSensorPosition()*600/(36000.0/5880.0)})
-            //     .velocity(units::meters_per_second_t{_drive_motor_FL.GetSelectedSensorVelocity()*600/(36000.0/5880.0)});
+            log->Motor("drive-bl")
+                .voltage(_drive_motor_BL.Get() *
+                         frc::RobotController::GetBatteryVoltage())
+                .position(units::meter_t{_drive_motor_BL.GetSelectedSensorPosition()*600/(36000.0/5880.0)})
+                .velocity(units::meters_per_second_t{_drive_motor_BL.GetSelectedSensorVelocity()*600/(36000.0/5880.0)});
+            log->Motor("drive-br")
+                .voltage(_drive_motor_BR.Get() *
+                         frc::RobotController::GetBatteryVoltage())
+                .position(units::meter_t{_drive_motor_BR.GetSelectedSensorPosition()*600/(36000.0/5880.0)})
+                .velocity(units::meters_per_second_t{_drive_motor_BR.GetSelectedSensorVelocity()*600/(36000.0/5880.0)});
           },
           this}};
 };
+
+
+
 
 
 #endif
